8d392bad2ddb1d5f9863de8629369a98fc050045
Попкова Екатерина Эдуардовна <EPopkova@alfabank.ru>
2023-05-17 01:13:45 +0300

    Initial commit for saga-example-on-zeebe-project

---
+++ saga-example-on-zeebe/src/test/java/com/saga/example/sagaexampleonzeebe/SagaExampleOnZeebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.example.sagaexampleonzeebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaExampleOnZeebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
